6a0d2cdb05c6d48e256bc67c2e78562a4f1eae9d
kelvin364 <k.rwihimba@alustudent.com>
2024-10-22 09:38:30 +0200

    commit

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,48 +1,25 @@
 #!/usr/bin/python3
 """
-This script fetches employee data from a public API (jsonplaceholder.typicode.com)
-and displays their completed tasks.
-
-Usage: python script.py <Employee_id>
+Fetches data from an API
+and returns information about the employee's todo list progress
 """
 
 import requests
-import sys
+from sys import argv
 
+if __name__ == '__main__':
+    userId = argv[1]
+    user = f"https://jsonplaceholder.typicode.com/users/{userId}"
+    todo = f"https://jsonplaceholder.typicode.com/todos?userId={userId}"
+    user_info = requests.get(user).json()
+    todos_info = requests.get(todo).json()
 
-def main(employee_id):
-    """
-    Main function that fetches user and tasks data from the API.
-    Prints the completed tasks for the given employee.
-    """
-    response_user = requests.get(f'https://jsonplaceholder.typicode.com/users/{employee_id}')
+    employee_name = user_info["name"]
+    task_completed = list(filter(lambda obj:
+                                 (obj["completed"] is True), todos_info))
+    number_of_done_tasks = len(task_completed)
+    total_number_of_tasks = len(todos_info)
 
-    if response_user.status_code == 200:
-        user_data = response_user.json()
-        employee_name = user_data['name']
-    else:
-        print("URL input error for user")
-        sys.exit(1)  # Exit if we can't fetch the user data
-    
-    response_todos = requests.get(f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}')
-
-    if response_todos.status_code == 200:
-        user_todo = response_todos.json()
-        completed_tasks = [task for task in user_todo if task['completed']]
-        total_tasks = len(user_todo)
-        completed_count = len(completed_tasks)
-        print(f"Employee {employee_name} is done with tasks ({completed_count}/{total_tasks}):")
-        for task in completed_tasks:
-            print(f"\t {task['title']}")
-    else:
-        print("URL input error for todos")
-        sys.exit(1)  # Exit if we can't fetch the todos data
-
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <Employee_id>")
-        sys.exit(1)
-
-    employee_id = int(sys.argv[1])
-    main(employee_id)
+    print("Employee {} is done with tasks({}/{}):".
+          format(employee_name, number_of_done_tasks, total_number_of_tasks))
+    [print("\t " + task["title"]) for task in task_completed]
