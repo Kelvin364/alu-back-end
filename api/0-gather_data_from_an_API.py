d760dd01343e6062b821c616b514e3408cd06e45
kelvin364 <k.rwihimba@alustudent.com>
2024-10-22 09:12:41 +0200

    commit

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,1 +1,36 @@
-#!usr/bin/python3
+#!/usr/bin/python3
+import requests
+import sys
+
+def main(Employee_id):
+    response_user = requests.get(f'https://jsonplaceholder.typicode.com/users/{Employee_id}')
+
+    if response_user.status_code == 200:
+        user_data = response_user.json()
+        Employee_name = user_data['name']
+    else:
+        print("URL input error for user")
+        sys.exit(1)  # Exit if we can't fetch the user data
+    
+    response_todos = requests.get(f'https://jsonplaceholder.typicode.com/todos/?userId={Employee_id}')
+
+    if response_todos.status_code == 200:
+        user_todo = response_todos.json()
+        to_do = [task for task in user_todo if task['completed']]
+        tasks = len(user_todo)
+        Completed_tasks = len(to_do)
+        print(f"Employee {Employee_name} is done with tasks ({Completed_tasks}/{tasks}):")
+        for task in to_do:
+            print(f"\t {task['title']}")
+    else:
+        print("URL input error for todos")
+        sys.exit(1)  # Exit if we can't fetch the todos data
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <Employee_id>")
+        sys.exit(1)
+
+    Employee_id = int(sys.argv[1])
+    main(Employee_id)
