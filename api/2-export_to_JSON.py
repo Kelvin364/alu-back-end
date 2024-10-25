99662c93abad20d6904ecfab8c009110104d645c
kelvin364 <k.rwihimba@alustudent.com>
2024-10-25 14:34:05 +0200

    commit

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,28 +1,33 @@
 #!/usr/bin/python3
-"""
-fetches data from an API
-and exports it to a JSON file
-"""
-
 import json
 import requests
 from sys import argv
 
+# Fetching API data and exporting user_info in a user.json file 
+
 if __name__ == '__main__':
-    userId = argv[1]
-    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".
-                        format(userId), verify=False).json()
-    todo = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
-                        format(userId), verify=False).json()
-    username = user.get('username')
-    tasks = []
-    for task in todo:
-        task_dict = {}
-        task_dict["task"] = task.get('title')
-        task_dict["completed"] = task.get('completed')
-        task_dict["username"] = username
-        tasks.append(task_dict)
-    jsonobj = {}
-    jsonobj[userId] = tasks
-    with open("{}.json".format(userId), 'w') as jsonfile:
-        json.dump(jsonobj, jsonfile)
+    # Mapping the URLs-endpoints and using the GET method
+    user_id = argv[1]
+    base_url = "https://jsonplaceholder.typicode.com/"
+    
+    # Fetch user data
+    user_data = requests.get(f"{base_url}users/{user_id}")
+    todos_data = requests.get(f"{base_url}todos?userId={user_id}")
+
+    # Convert fetched data to JSON format
+    user = user_data.json()
+    user_todo = todos_data.json()
+    employee_name = user["name"]
+
+    # Prepare data to export to JSON format
+    export_data = {
+        user_id: [{
+            "username": employee_name,
+            "task": task["title"],
+            "completed": task["completed"]
+        } for task in user_todo]
+    }
+
+    # Save data to JSON file
+    with open(f"{user_id}.json", 'w') as json_file:
+        json.dump(export_data, json_file, indent=4)
